2694974c508c0510c8e93c136cd8925fc6be430c
ruturaj warunkshe <ruturajwarunkshe@gmail.com>
2024-08-07 12:21:31 +0530

    modified code

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI,{dbName:'baliraja'});
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log('Database connected');
   } catch (error) {
     console.error('MongoDB connection error:', error);
